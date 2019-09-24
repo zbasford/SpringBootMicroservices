022bf1bfd046e1db04262d626c388b141cd671fb
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-24 17:07:50 -0400

    controller precedence headers/params

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Administrator
@@ -12,12 +14,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 
-	@RequestMapping("/start")
-	public String start()
+	@RequestMapping(value="/start", method = RequestMethod.GET)
+	public String read()
+	{
+		return "start";
+	}
+	@RequestMapping(value="/start", method = RequestMethod.GET, headers = {"content-type=text/json"})
+	public String readJSON()
+	{
+		return "start";
+	}
+	@RequestMapping(value="/start", method = RequestMethod.GET, params = {"loyalty=blue"})
+	public String readBlue()
+	{
+		return "start";
+	}
+	@RequestMapping(value="/start", method = RequestMethod.GET, params = {"loyalty=silver"})
+	public String readSilver()
 	{
 		return "start";
 	}
 	
+	@PostMapping("/start")
+	public String create()
+	{
+		return "start";
+	}	
 	/**
 	 * Handle the / endpoint
 	 *@return
